9b48ef2e9d2b52c6950e3f8ff247baaef46d7595
lswang <lswang@wisedu.com>
2018-08-10 13:31:30 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import wisDialog from './components/pc/wisDialog.vue'
+import wisSelectDept from './components/pc/wisSelectDept.vue'
+import wisSelectDeptUser from './components/pc/wisSelectDeptUser.vue'
+import wisTree from './components/pc/wisTree.vue'
 
 export {
-  wisDialog
+  wisDialog,
+  wisSelectDept,
+  wisSelectDeptUser,
+  wisTree
 }
