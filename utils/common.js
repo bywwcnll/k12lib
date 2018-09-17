0348c0c6df233f56af75ba6f985c6582183bf4dc
lswang <lswang@wisedu.com>
2018-09-17 16:39:31 +0800

    更新common方法

--- utils/common.js
+++ utils/common.js
@@ -1,7 +1,20 @@
 export const downloadFile = async ({action = '', params = {}, method = 'GET'}) => {
+  if (action.indexOf('?') > -1) {
+    let queryStr = action.slice(action.indexOf('?') + 1) || ''
+    if (queryStr.length > 0) {
+      let queryParamsArray = queryStr.split('&')
+      queryParamsArray.forEach(el => {
+        let kw = el.split('=')
+        if (kw.length === 2) {
+          let tmp = {}
+          tmp[kw[0]] = kw[1]
+          params = {...tmp, ...params}
+        }
+      })
+    }
+  }
   let form = window.document.createElement('form')
-  form.id = 'downloadFileForm'
-  form.name = 'downloadFileForm'
+  form.id = form.name = 'downloadFileForm'
   form.action = action
   form.method = method
   window.document.body.appendChild(form)
@@ -16,7 +29,21 @@ export const downloadFile = async ({action = '', params = {}, method = 'GET'}) =
   window.document.body.removeChild(form)
 }
 
-export const deepCopyObj = (data) => JSON.parse(JSON.stringify(data))
+export const deepClone = (data) => {
+  let result = Array.isArray(data) ? [] : {}
+  if (data && typeof data === 'object') {
+    for (let key in data) {
+      if (data.hasOwnProperty(key)) {
+        if (data[key] && typeof data[key] === 'object') {
+          result[key] = deepClone(data[key])
+        } else {
+          result[key] = data[key]
+        }
+      }
+    }
+  }
+  return result
+}
 
 export const mapDeptData = (data = [], disabled = false) => {
   return data.map(el => {
